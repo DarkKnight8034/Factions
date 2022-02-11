43bb66ae4522791387f31a60c079e921a805fbb1
DarkKnight8034 <darkknight8034@gmail.com>
2022-02-11 18:26:39 -0500

    block break event

--- factions/src/main/java/io/github/darkknight8034/factions/EventListener.java
+++ factions/src/main/java/io/github/darkknight8034/factions/EventListener.java
@@ -10,9 +10,10 @@ import org.bukkit.event.block.BlockBreakEvent;
 // Bukkit
 import org.bukkit.entity.Player;
 import org.bukkit.Chunk;
+import org.bukkit.ChatColor;
 
 // Java
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -36,7 +37,50 @@ public class EventListener implements Listener
     public void blockBreakEvent(BlockBreakEvent event)
     {
 
-        
+        String faction = Main.plugin.dataFile.getString("players." + event.getPlayer().getName());
+
+        // Player is in a faction, territories must be checked
+        if (faction != null)
+        {
+
+            // Gets string version of chunk data
+            String chunk = event.getBlock().getChunk().getX() + "," + event.getBlock().getChunk().getZ();
+
+            // Not in their own territory
+            if (!Main.plugin.dataFile.getList("factions." + faction + ".territory").contains(chunk))
+            {
+
+                List<String> factions = (List<String>) Main.plugin.dataFile.getList("factions." + faction + ".enemies");
+            
+                // Not in any wars
+                if (factions.size() == 0)
+                {
+
+                    event.setCancelled(true);
+                    event.getPlayer().sendMessage(ChatColor.RED + "You aren't allowed to do that here!");
+                
+                }
+                else // checks if enemies have chunk
+                {
+
+                    for (String f: factions)
+                    {
+
+                        // Found chunk, done search
+                        if (Main.plugin.dataFile.getList("factions." + f + ".territory").contains(chunk))
+                        {
+
+                            break;
+                        
+                        }
+                    
+                    }
+                    
+                }
+
+            }
+
+        }
 
     }
 
